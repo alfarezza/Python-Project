299f3eb124cd34ca0e68bf609e91814ee117a61f
alfarezza <alfarezza08@gmail.com>
2025-07-22 09:57:17 +0700

    Add code calcultation working and absent

--- Tools/work_time.py
+++ Tools/work_time.py
@@ -11,8 +11,17 @@ jam_masuk = datetime.strptime(jam_masuk_str, fmt)
 jam_keluar = datetime.strptime(jam_keluar_str, fmt)
 
 # Hitung selisih waktu
+durasi_hadir = jam_keluar - jam_masuk
+durasi_hadir -= timedelta(hours=1)
+
 durasi_kerja = jam_keluar - jam_masuk
 durasi_kerja -= timedelta(hours=0.5)
+
+# Tampilkan durasi dalam jam, menit, dan detik
+total_detik = durasi_hadir.total_seconds()
+jam_hadir = int(total_detik // 3600)
+menit_hadir = int((total_detik % 3600) // 60)
+detik_hadir = int(total_detik % 60)
 
 # Tampilkan durasi dalam jam, menit, dan detik
 total_detik = durasi_kerja.total_seconds()
@@ -20,4 +29,6 @@ jam = int(total_detik // 3600)
 menit = int((total_detik % 3600) // 60)
 detik = int(total_detik % 60)
 
-print(f"Lama waktu kerja: {jam} jam {menit} menit {detik} detik")
+
+print(f"Total waktu kehadiran: {jam_hadir} jam {menit_hadir} menit {detik_hadir} detik")
+print(f"Total waktu kerja: {jam} jam {menit} menit {detik} detik")
