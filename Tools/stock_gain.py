68e1ebb75a96a02401787a3f6a3735607c0c30f4
alfarezza <alfarezza08@gmail.com>
2025-07-22 13:21:15 +0700

    Add Percentage Gain

--- Tools/stock_gain.py
+++ Tools/stock_gain.py
@@ -5,7 +5,8 @@ def hitung_gain(jumlah_lot, harga_beli, harga_sekarang):
     gain_per_lembar = harga_sekarang - harga_beli
     total_gain = gain_per_lembar * jumlah_lembar
     total_aset = harga_sekarang * jumlah_lembar  # atau: harga_beli_awal + total_gain
-    return jumlah_lembar, harga_beli_awal, total_gain, total_aset
+    persentase_gain = (total_gain / harga_beli_awal) * 100
+    return jumlah_lembar, harga_beli_awal, total_gain, total_aset, persentase_gain
 
 # Ambil input dari pengguna
 try:
@@ -14,7 +15,7 @@ try:
     harga_sekarang = int(input("Masukkan harga saham saat ini per lembar (misal 1551): "))
 
     # Hitung dan tampilkan hasil
-    jumlah_lembar, harga_beli_awal, gain, total_aset = hitung_gain(jumlah_lot, harga_beli, harga_sekarang)
+    jumlah_lembar, harga_beli_awal, gain, total_aset, persentase_gain = hitung_gain(jumlah_lot, harga_beli, harga_sekarang)
 
     print("\n=== Hasil Perhitungan ===")
     print(f"Jumlah lot         : {jumlah_lot}")
@@ -23,6 +24,7 @@ try:
     print(f"Total harga beli   : Rp {harga_beli_awal:,}")
     print(f"Harga sekarang     : Rp {harga_sekarang:,}")
     print(f"Total gain         : Rp {gain:,}")
+    print(f"G/L Percentage     : {persentase_gain:.2f} %")
     print(f"Total nilai aset   : Rp {total_aset:,}")
 
 except ValueError:
