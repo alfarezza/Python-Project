155c5dab21d2616f9df48087396938f101ec6bf6
alfarezza <alfarezza08@gmail.com>
2025-07-23 10:47:31 +0700

    Updated

--- ML_Apps/trial-app.py
+++ ML_Apps/trial-app.py
@@ -2,5 +2,7 @@ import streamlit as st
 import pandas
 import os
 
+# ================================= Page =================================
+st.set_page_config(page_title="Trial-Apps", page_icon="🤖", layout="wide")
 
 st.write("Hello Word")
