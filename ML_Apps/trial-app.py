8a8846122b0579427330732eac95f29683fe6d55
alfarezza <alfarezza08@gmail.com>
2025-07-23 10:56:00 +0700

    Updated Upload File

--- ML_Apps/trial-app.py
+++ ML_Apps/trial-app.py
@@ -1,8 +1,25 @@
 import streamlit as st
-import pandas
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import os
 
 # ================================= Page =================================
 st.set_page_config(page_title="Trial-Apps", page_icon="🤖", layout="wide")
 
-st.write("Hello Word")
+st.title("Simple Data Dashboard 📊")
+
+# ================================= Side Menu =================================
+st.sidebar.title("📂 Main Menu!")
+page = st.sidebar.radio('Choose Page:', ['Data Understanding', 'Exploratory Data Analysis', 'About Me'])
+
+
+# ================================= Menu Data Understanding =================================
+if page == 'Data Understanding':
+    load_file = st.file_uploader("Choose a CSV file", type="csv")
+
+    if load_file is not None:
+        df = pd.read_csv(load_file)
+
+        st.subheader("Data Preview 📅")
+        st.write(pd.head(10))
